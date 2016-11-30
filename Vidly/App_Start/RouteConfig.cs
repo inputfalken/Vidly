4abce5ac86e2e79a44a03a705dbcbe91763fe0c7
Robert Andersson <inputfalken@gmail.com>
2016-11-30 20:38:04 +0100

    Adds custom routing.

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -5,18 +5,22 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Vidly
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
+namespace Vidly {
+    public class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "MoviesByReleaseDate",
+                url: "movies/released/{year}/{month}",
+                defaults: new {controller = "Movies", action = "ByReleaseDate"},
+                constraints: new {year = @"2015|2016", month = @"\d{1}|\d{2}"}
+            );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
             );
         }
     }
--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -6,14 +6,31 @@ using System.Web.Mvc;
 using Vidly.Models;
 
 namespace Vidly.Controllers {
-    public class MovieController : Controller {
+    public class MoviesController : Controller {
         // GET: Movies
         public ActionResult Random() {
             var movie = new Movie() {
                 Name = "Shrek!"
             };
-
             return View(movie);
+        }
+
+        public ActionResult Edit(int id) {
+            return Content("id=" + id);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month) {
+            return Content(year + "/" + month);
+        }
+
+        //Movies
+        public ActionResult Index(int? pageIndex, string sortBy) {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
         }
     }
 }
