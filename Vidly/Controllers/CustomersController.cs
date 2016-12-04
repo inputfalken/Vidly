1eb338766675e05fbc2be90ff9bada9b0c89b7f1
Robert Andersson <inputfalken@gmail.com>
2016-12-04 15:54:12 +0100

    adds Customer index controller.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
-namespace Vidly.Controllers
-{
-    public class CustomersController : Controller
-    {
+namespace Vidly.Controllers {
+    public class CustomersController : Controller {
+        private static readonly IEnumerable<Customer> Customers = new[] {
+            new Customer {
+                Name = "Joe",
+                Id = 1
+            },
+            new Customer {
+                Name = "Bob",
+                Id = 2
+            },
+        };
+
+
         // GET: Customers
-        public ActionResult Index()
-        {
-            return View();
+        public ActionResult Index() {
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id) {
+            throw new NotImplementedException();
         }
     }
 }
